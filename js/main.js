16f54c47ee6990f3533449cfb128cab151a8063b
cfjedimaster <raymondcamden@gmail.com>
2012-10-04 09:39:41 -0500

    Next major version

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
 Parse.initialize("oe3dboiG0RzJNULxKvdHYGWEb3Cq7mzHRC3Dwh6E", "cR8whmMjybMoXUqfAzhxUJSiBXw3QPt7ZB4bRGw8");
 TipObject = Parse.Object.extend("TipObject");
 
+var currentLocation;
+
 $(document).ready(function() {
+
+	//I handle doing GPS on addForm display
+    if($("#addTipBtn").length === 1) {
+		currentLocation=null;
+		navigator.geolocation.getCurrentPosition(function(pos) {
+			//store the long/lat
+			currentLocation = {longitude:pos.coords.longitude, latitude:pos.coords.latitude};
+			$("#addTipBtn").removeAttr("disabled");
+		}, function(err) {
+			//Since geolocation failed, we can't allow the user to submit
+			doAlert("Sorry, but we couldn't find your location.\nYou may not post a cow tip.");
+		});
+
+    }
 
     $("#addtipForm").on("submit", function(e) {
 		e.preventDefault();
@@ -13,11 +29,13 @@ $(document).ready(function() {
 
 		//TBD: Validation
 		var tip = new TipObject();
+		var point = new Parse.GeoPoint({latitude: currentLocation.latitude, longitude: currentLocation.longitude});
 		tip.save(
 				{
 					numcows:numcows,
 					howdangerous:howdangerous,
-					comments:comments
+					comments:comments,
+					location:point
 				},{
 					success:function(object) {
 						console.log("Saved object");
@@ -32,32 +50,52 @@ $(document).ready(function() {
 
     if($("#tipdisplay").length === 1) {
 
-		var query = new Parse.Query(TipObject);
-		query.find({
-			success: function(results) {
-				var s = '';
-				for(var i=0, len=results.length; i<len; i++) {
-					var result = results[i];
-					console.dir(result);
-					s+= '<p>';
-					s+= '<b>ID:</b> '+ result.id + '<br/>';
-					s+= 'Created: ' + result.createdAt + '<br/>';
-					s+= 'Number of Cows: ' + result.attributes.numcows + '<br/>';
-					s+= 'How Dangerous?: ' + result.attributes.howdangerous + '<br/>';
-					s+= 'Comments: ' + result.attributes.comments;
-					s+= '</p>';
-				}
-				$("#tipdisplay").html(s);
-			},
-			error: function(error) {
-				alert("Error: " + error.code + " " + error.message);
-			}
-		}); 
+		//Update status to let the user know we are doing important things. Really important.
+		$("#tipdisplay").html("Please stand by. Checking your location for nearby cow tips!");
+
+		navigator.geolocation.getCurrentPosition(function(pos) {
+			var myLocation = new Parse.GeoPoint({latitude: pos.coords.latitude, longitude: pos.coords.longitude});
+
+			//Begin our query
+			var query = new Parse.Query(TipObject);
+			//query.near("location", myLocation);
+			//Only within 30 miles
+			query.withinMiles("location", myLocation, 30);
+			//only within last week
+			var lastWeek = new Date();
+			lastWeek.setDate(lastWeek.getDate()-7);
+			query.greaterThan("createdAt", lastWeek);
+			query.find({
+				success:function(results) { renderResults(results,myLocation); },
+				error: function(error) { alert("Error: " + error.code + " " + error.message); }
+			});
+
+		}, function(err) {
+			//Since geolocation failed, we can't allow the user to submit
+			doAlert("Sorry, but we couldn't find your location.");
+		},{timeout:20000,enableHighAccuracy:false});
     }
 });
 
+function renderResults(results,myLoc) {
+	console.log("renderResults: "+results.length);
+
+	$("#tipdisplay").html("Displaying tips within 30 miles and from the last 7 days.");
+
+	var map = L.map('map').setView([myLoc.latitude, myLoc.longitude], 8);
+	var layerOpenStreet = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom:18, minZoom:1, attribution:'Map data &copy; 2012 OpenStreetMap'}).addTo(map);
+	var dangerLevels = ["Totally Safe","Some Risk","Farmer with Shotgun!"];
+
+	for(var i=0, len=results.length; i<len; i++) {
+		var result = results[i];
+		var marker = L.marker([result.attributes.location.latitude, result.attributes.location.longitude]).addTo(map);
+		var markerLabel = "Cows: "+result.attributes.numcows+"<br/>Danger: "+dangerLevels[result.attributes.howdangerous-1];
+		if(result.attributes.comments && result.attributes.comments.length) markerLabel += "<br>"+result.attributes.comments;
+		marker.bindPopup(markerLabel);
+	}
+}
+
 //Wrapper for alert so I can dynamically use PhoneGap alert's on device
 function doAlert(str,cb) {
-	alert(str);
-	cb();
+	if(cb) cb();
 }
